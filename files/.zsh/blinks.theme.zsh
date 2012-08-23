427cac3b0cb4f16ca29bb27bf92a6dcbb82b55b2
Alan Sherman <alan.sherman@commerceguys.com>
2012-08-23 11:37:14 +0200

    Removing experimental code will move to its a dev branch

--- files/.zsh/blinks.theme.zsh
+++ files/.zsh/blinks.theme.zsh
@@ -13,17 +13,11 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{black}%B%F{green}%}]"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-function user_color() {
-if ((${USER} == root)); then
-    echo "%B%F{red}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%}"
-else
-    echo "%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%}"
-fi
-}
+ZSH_PROMPT_USER "%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%}"
 ZSH_PROMPT_PWD="%{%b%F{yellow}%K{black}%}%~%{%B%F{green}%}"
 
 PROMPT='%{%f%k%b%}
-%{%K{black}$(user_color) ${ZSH_PROMPT_PWD} $(git_prompt_info)%E%{%f%k%b%}
+%{%K{black}$(user_color) ${ZSH_PROMPT_PWD} ${ZSH_PROMPT_USER}%E%{%f%k%b%}
 %{%K{black}%}$(_prompt_char)%{%K{black}%} %#%{%f%k%b%} '
 
 RPROMPT=%B%F{white}%t%{%f%k%b%}
